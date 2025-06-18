f59d5a3063cdba88e3260c2d76def8f8d78520bc
sergio129 <sanayaromero62@gmail.com>
2025-06-18 13:57:35 -0500

    ajuste estado de los proyectos

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -86,12 +86,10 @@ export default function Testimonials() {
               {/* Quote Icon */}
               <svg className="h-12 w-12 text-blue-600 mx-auto mb-6" fill="currentColor" viewBox="0 0 24 24">
                 <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h4v10h-10z"/>
-              </svg>
-
-              {/* Testimonial Text */}
+              </svg>              {/* Testimonial Text */}
               <blockquote className="text-xl md:text-2xl text-gray-700 leading-relaxed mb-8 italic">
-                "{testimonials[currentTestimonial].testimonial}"
-              </blockquote>              {/* Stars Rating */}
+                &ldquo;{testimonials[currentTestimonial].testimonial}&rdquo;
+              </blockquote>{/* Stars Rating */}
               <div className="flex justify-center mb-6">
                 {[...Array(testimonials[currentTestimonial].rating)].map((_, starIndex) => (
                   <svg key={`${testimonials[currentTestimonial].id}-star-${starIndex}`} className="h-6 w-6 text-yellow-400" fill="currentColor" viewBox="0 0 24 24">
